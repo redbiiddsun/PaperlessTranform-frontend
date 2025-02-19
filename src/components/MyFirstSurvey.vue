ee34474d4da3bb22cea9b6b7574c566a44b57d88
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2025-02-19 21:34:26 +0700

    fix: remove unnecessary inputType element and refactor

--- src/components/MyFirstSurvey.vue
+++ src/components/MyFirstSurvey.vue
@@ -2,64 +2,57 @@
 import 'survey-core/defaultV2.min.css'
 import { Model } from 'survey-core'
 import { SurveyComponent } from 'survey-vue3-ui'
-import { computed, ref, watchEffect } from 'vue'
+import { ref, watchEffect } from 'vue'
 
 // const SURVEY_ID = 1;
-const tempData = {
-  element: [
-    {
-      orginalField: "ชื่อ",
-      field: "name",
-      type: "text"
-    },  {
-      orginalField: "อายุ",
-      field: "age",
-      type: "text",
-      inputType: "number"
-    },  {
-      orginalField: "ตำบล",
-      field: "district",
-      type: "text"
-    },  {
-      orginalField: "วันเกิด",
-      field: "Birthdate",
-      type: "text",
-      inputType: "date"
-    },  {
-      orginalField: "รหัสไปรณีย์",
-      field: "postal_code",
-      type: "text"
-    },
-  ]
+
+interface ITempData {
+  orginalField: string
+  field: string
+  type: string
 }
 
-const surveyJson = ref({
-  elements: tempData.element.map(item => ({
-    name: item.field,
-    title: `Enter your ${item.orginalField}:`,
-    type: item.type,
-    ...(item.inputType ? { inputType: item.inputType } : {})
-  }))
-});
+const tempData: ITempData[] = [
+  {
+    orginalField: 'ชื่อ',
+    field: 'name',
+    type: 'text',
+  },
+  {
+    orginalField: 'อายุ',
+    field: 'age',
+    type: 'number',
+  },
+  {
+    orginalField: 'ตำบล',
+    field: 'district',
+    type: 'text',
+  },
+  {
+    orginalField: 'วันเกิด',
+    field: 'Birthdate',
+    type: 'date',
+  },
+  {
+    orginalField: 'รหัสไปรณีย์',
+    field: 'postal_code',
+    type: 'text',
+  },
+]
 
-console.log("Survey JSON:", JSON.stringify(surveyJson.value, null, 2));
+function transformData(data: ITempData[]): { elements: { name: string; type: string }[] } {
+  return {
+    elements: data.map(({ orginalField, type }) => ({
+      name: orginalField,
+      type: 'text',
+      inputType: type,
+    })),
+  }
+}
 
-// console.log("Survey JSON:", JSON.stringify(surveyJson.value, null, 2));
+const surveyJson = transformData(tempData)
 
-// const surveyJson = {
-//   elements: [
-//     {
-//       name: 'FirstName',
-//       title: 'Enter your first name:',
-//       type: 'text',
-//     },
-//     // {
-//     //   name: 'LastName',
-//     //   title: 'Enter your last name:',
-//     //   type: 'text',
-//     // },
-//   ],
-// }
+console.log('Survey JSON:', JSON.stringify(surveyJson, null, 2))
 
 const alertResults = (sender: any) => {
   const results = JSON.stringify(sender.data)
@@ -72,16 +65,16 @@ const alertResults = (sender: any) => {
 
 // const survey = new Model(surveyJson)
 // survey.onComplete.add(alertResults)
-const survey = ref(new Model(surveyJson.value));
+const survey = ref(new Model(surveyJson))
 watchEffect(() => {
-  survey.value = new Model(surveyJson.value);
-});
+  survey.value = new Model(surveyJson)
+})
 
-console.log("Survey Model:", survey.value);
+console.log('Survey Model:', survey.value)
 
 survey.value.onComplete.add((sender: any) => {
-  alert(JSON.stringify(sender.data));
-});
+  alert(JSON.stringify(sender.data))
+})
 // function saveSurveyResults(url: string | URL, json: object) {
 //   fetch(url, {
 //     method: 'POST',
@@ -104,6 +97,5 @@ survey.value.onComplete.add((sender: any) => {
 </script>
 
 <template>
-  <!-- <SurveyComponent :model="survey" /> -->
   <SurveyComponent :model="survey" :key="survey" />
 </template>
