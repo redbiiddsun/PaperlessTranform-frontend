a116bff161edbdc0bc89793ff6aedda52be731f7
B0wiz <Pra_pakorn910@hotmail.com>
2025-03-03 15:54:19 +0700

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1A74E2',
+        secondary: '#3FA0EF',
+        highlight: '#6BD0FF',
+        text: '#FFFFFF',
+        subtext: '#434649',
+        text_b: '#light:text',
+        success: '#28A745',
+        warning: '#FFC107',
+        error: '#DC3545',
+      },
+    },
+  },
+  plugins: [],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    open: true,
+  }
 })
