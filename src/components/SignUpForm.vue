4af799d9eecab6de22edff2d9ad2290d664cd414
B0wiz <Pra_pakorn910@hotmail.com>
2025-03-20 15:11:22 +0700

    feat:Auth error handdel

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -4,7 +4,8 @@ import { RouterLink } from 'vue-router'
 import AuthError from '@/components/AuthError.vue'
 
 const user = ref({
-  name: '',
+  fname: '',
+  lname: '',
   email: '',
   password: '',
   confirmPassword: '',
@@ -16,7 +17,7 @@ const signup = () => {
 }
 
 const err = ref('')
-const showError = ref(false) 
+const showError = ref(false)
 
 const validatePasswords = () => {
   if (user.value.password !== user.value.confirmPassword) {
@@ -26,12 +27,11 @@ const validatePasswords = () => {
     err.value = ''
   }
 }
-
 </script>
 
 <template>
   <div
-    class="flex flex-col justify-center items-center w-full h-screen md:h-fit xl:h-screen gap-1 md:gap-2 p-8 md:px-28 md:py-16 xl:py-8 md:rounded-2xl xl:rounded-none md:bg-text md:border-4 md:border-hightlight xl:border-none"
+    class="flex flex-col justify-center items-center w-full h-screen md:h-fit xl:h-screen gap-1 md:gap-2 p-8 md:px-24 md:py-16 xl:py-8 md:rounded-2xl xl:rounded-none md:bg-text md:border-4 md:border-hightlight xl:border-none"
   >
     <!-- Logo -->
     <div alt="" class="w-[200px] h-[200px] bg-[#EAEAEA] rounded-lg"></div>
@@ -45,18 +45,34 @@ const validatePasswords = () => {
 
     <!-- Form -->
     <div class="flex flex-col gap-6 px-2 py-2 md:py-2 w-full h-fit">
+
       <!-- Name Input -->
-      <div class="flex flex-col gap-1">
-        <p class="font-Poppins font-bold text-24 text-text_b">Name</p>
-        <div
-          class="flex flex-col gap-0 px-2 py-3 w-80 md:w-full border-b-4 border-secondary items-center"
-        >
-          <input
-            v-model="user.name"
-            type="text"
-            class="font-Poppins font-normal text-16 placeholder-subtext bg-transparent outline-none w-full"
-            placeholder="Nattawut Pianok"
-          />
+      <div class="flex flex-row w-full gap-16">
+        <div class="flex flex-col gap-1 w-full">
+          <p class="font-Poppins font-bold text-24 text-text_b">First Name</p>
+          <div
+            class="flex flex-col gap-0 px-2 py-3 w-80 md:w-full border-b-4 border-secondary items-center"
+          >
+            <input
+              v-model="user.fname"
+              type="text"
+              class="font-Poppins font-normal text-16 placeholder-subtext bg-transparent outline-none w-full"
+              placeholder="FirstName"
+            />
+          </div>
+        </div>
+        <div class="flex flex-col gap-1  w-full">
+          <p class="font-Poppins font-bold text-24 text-text_b">Last Name</p>
+          <div
+            class="flex flex-col gap-0 px-2 py-3 w-80 md:w-full border-b-4 border-secondary items-center"
+          >
+            <input
+              v-model="user.lname"
+              type="text"
+              class="font-Poppins font-normal text-16 placeholder-subtext bg-transparent outline-none w-full"
+              placeholder="LastName"
+            />
+          </div>
         </div>
       </div>
 
@@ -123,7 +139,7 @@ const validatePasswords = () => {
     <div class="flex flex-row md:w-full gap-4 justify-center items-center">
       <button
         @click="signup"
-        :disabled="!user.name || !user.email || !user.password || !user.confirmPassword"
+        :disabled="!user.fname || !user.lname || !user.email || !user.password || !user.confirmPassword"
         class="flex flex-col justifyitems-center items-center gap-0 py-2 px-12 bg-gradient-to-tr from-primary to-hightlight rounded-2xl font-bold text-text cursor-pointer disabled:border disabled:border-subtext/70 disabled:bg-none disabled:font-normal disabled:text-subtext/70 disabled:cursor-not-allowed w-full lg:w-3/4"
       >
         <p class="font-Poppins text-2xl tracking-[0.1em]">Sign Up</p>
