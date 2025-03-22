48ae13224dd7669ef00dd5c1d7f2eac86061e69a
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2025-03-22 12:43:08 +0700

    refactor: comment out unused MyFirstSurvey component and HelloWorld tests

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import MyFirstSurvey from '@/components/MyFirstSurvey.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <RouterView />
-  <!-- <MyFirstSurvey /> -->
 </template>
